3b1c72c3c44fcde880f9c29bb20993bea0e20a65
luis fernandez <fernandezluisn@gmail.com>
2020-04-04 14:45:24 -0300

    laborde

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express=require('express');
-const app=express();
-const path=require('path');
-
-app.use(express.static(__dirname+'/angularapp'));
-
-app.listen(process.env.port || 8080);
-
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(__dirname + '/angularapp'));
+app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
-
-console.log('exito');
+console.log('Build successful!!');
